20b2e92bfc1bfbb30ac8eedaeb4d8f8410174295
Cindy Chen <chen_cx0128@hotmail.com>
2019-05-31 05:36:03 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,73 +1,82 @@
 # your code goes here
-def begins_with_r(tools)
-tools.all? do |tool|
-tool.downcase.chr == "r"
- end
+
+def organize_schools(schools)
+  organized_schools = {}
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+    else
+      organized_schools[location] = []
+      organized_schools[location] << name
+    end
+  end
+  organized_schools
+end
+
+def begins_with_r(array)
+  flag = true
+  array.each do |element|
+    flag = false if element[0] != "r"
+  end
+  flag
 end
 
 def contain_a(array)
-  array.select do |word|
-      word.downcase.include?("a")
+  container = []
+  array.each do |element|
+    container << element if element.include?("a")
   end
-end
-
-def first_wa(array)
-    array.detect do |word|
-    word[0,2] == "wa"
-  end
-  #return word
+  container
 end
 
 def remove_non_strings(array)
-  #if array.is_a?(String)
-  array.delete_if do |string|
-  string.is_a?(String) == false
+  container = []
+  array.each do |element|
+    container << element if element.is_a?(String)
   end
+  container
 end
 
-def count_elements(array)
-  #new_array = []
-   array.each do |item|
-    item[:count] = 0
-    name = item[:name]
-    array.each do |data|
-      if data[:name] == name
-        item[:count] += 1
-      end
+def first_wa(array)
+  first_wa = nil
+  array.each do |element|
+    if element.match(/wa/)
+      first_wa = element
+      break
     end
   end
-    return array.uniq
-end
-
-def merge_data(keys, data)
-  array = []
-  keys.each do |key_hash|
-    data.each do |data_hash|
-      data_hash.each do |name, info|
-        if key_hash.values[0] == name
-          array << key_hash.merge(info)
-        end
-      end
-    end
-  end
-  array
+  first_wa
 end
 
 def find_cool(array)
-  cool_stuff = []
-  array.each do |hash|
-    hash.each do |key, value|
-      if (key == :temperature && value == "cool")
-        cool_stuff << hash
-      end
-    end
+  container = []
+  array.each do |element|
+    container << element if element[:temperature] == "cool"
   end
-  cool_stuff
+  container
 end
 
-def organize_schools(hash)
-  locations = {}
-  hash.values.each { |location| locations[location.values[0]] = [] }
-  hash.each { |school, location| locations[location.values[0]] << school }
-  locations
-end 
+def count_elements(array)
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
+end
+
+def merge_data(arr1, arr2)
+  arr2[0].map do |name, prop_hash|
+    new_prop_hash = {}
+    arr1.each do |new_attr_hash|
+      if new_attr_hash[:first_name] == name
+        new_prop_hash = prop_hash.merge(new_attr_hash)
+      end
+    end
+    new_prop_hash
+  end
+end
